b103b03b6c929e7e26d2bdabebd89de2600f21c2
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-03-01 00:09:25 +0300

    add license content

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
-$('.main').click(function () {
-    $(this).children('.hide').slideToggle(1200);
-    $(this).children('.show').toggle(1500);
-  });
-  
+$(document).ready(function() {
+    $("p").click(function() {
+      $(".clickable").click(function() {
+      $(".show").toggle();
+      $(".hide").toggle();
+    });
+});
