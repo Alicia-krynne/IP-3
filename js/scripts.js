8fc2403ac97c350c9b4bf49cdcb562b0d0506ca0
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-03-03 00:19:49 +0300

    added bootstrap files

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,4 @@
-$(document).ready(function() {
-  $('.desClick').click(function () {
-    $(".desShow").Toggle ();
-    $(".desHidden").Toggle ();
-  });
-});
-
-$(document).ready(function() {
-  $('.devClick').click(function () {
-    $(".devShow").Toggle ();
-    $(".devHidden").Toggle ();
-  });
-});
-
-$(document).ready(function() {
-  $('.prodClick').click(function () {
-    $(".prodShow").Toggle ();
-    $(".prodHidden").Toggle ();
-  });
+$('.main').click(function () {
+  $(this).children('.hide').slideToggle();
+  $(this).children('.show').toggle();
 });
