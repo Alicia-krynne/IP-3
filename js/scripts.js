f121693f795e98eab260d66971411a86ba1457b8
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-03-02 08:51:50 +0300

    js not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,34 @@
+
 $(document).ready(function() {
-    $("p").click(function() {
-      $(".clickable").click(function() {
-      $(".show img-fluid").toggle();
-      $(".hide").toggle();
-   
+  $(".btn btn-primary").click(function(event) {
+    whatToSay = "Thank you  for your message!";
+    alert(whatToSay);
+  });
+
+  });
+
+$(document).ready(function() {
+  $('.main').click(function () {
+  $(this).children('.hide').slideToggle(1200);
+  $(this).children('.show').toggle(1500);
 });
-e.addEventListener("click", myFunction);
-function myFunction() {
-  alert ("THANK YOU  FOR YOUR RESPONSE");
+
+$(document).ready(function() {
+  $("button").click(function (event) {
+  var client = document.getElementById('nameDetail').value;
+  alert(' Dear ' + ' esteemed ' + client + ' we have received your message successful. ' + ' Feel free to reach out to us anytime');
+
 });
+$(document).ready(function() {
+  $("button").on('click', function () {
+  $('form').each(function () {
+    this.reset();
+  });
+});
+
+$(".card").hover(function () {
+  $(this).children(".card-1").fadeToggle(1000, "linear");
+});
+
+
+e.preventDefault();
