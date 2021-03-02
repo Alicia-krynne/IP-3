fe1b9f5bb616295bb78accccde6c993ddf1b41a8
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-03-02 20:57:27 +0300

    added mailchimp

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,20 @@
-
 $(document).ready(function() {
-  $(".btn btn-primary").click(function(event) {
-    whatToSay = "Thank you  for your message!";
-    alert(whatToSay);
-  });
-
-  });
-
-$(document).ready(function() {
-  $('.main').click(function () {
-  $(this).children('.hide').slideToggle(1200);
-  $(this).children('.show').toggle(1500);
-});
-
-$(document).ready(function() {
-  $("button").click(function (event) {
-  var client = document.getElementById('nameDetail').value;
-  alert(' Dear ' + ' esteemed ' + client + ' we have received your message successful. ' + ' Feel free to reach out to us anytime');
-
-});
-$(document).ready(function() {
-  $("button").on('click', function () {
-  $('form').each(function () {
-    this.reset();
+  $('.desClick').click(function () {
+    $(".desShow").Toggle ();
+    $(".desHidden").Toggle ();
   });
 });
 
-$(".card").hover(function () {
-  $(this).children(".card-1").fadeToggle(1000, "linear");
+$(document).ready(function() {
+  $('.devClick').click(function () {
+    $(".devShow").Toggle ();
+    $(".devHidden").Toggle ();
+  });
 });
 
-
-e.preventDefault();
+$(document).ready(function() {
+  $('.prodClick').click(function () {
+    $(".prodShow").Toggle ();
+    $(".prodHidden").Toggle ();
+  });
+});
